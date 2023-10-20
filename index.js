f52f93168a405449ec7b824f7528a05c377f1ed5
desyasarbini <desya.sarbini@gmail.com>
2023-10-20 20:00:31 +0800

    update

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
-var myName1;
-let myName2;
-const myName3 = "cici";
-
-console.log(myName1, myName2, myName3);
-
-myName1 = "desya";
-myName2 = "eci";
-// myName3 = "cici2";
-console.log(myName1, myName2, myName3);
-
-// camel case
-let myNameInAnotherCountry = 1;
-
-//snake case
-let my_name_in_another_country;
-
-const $ = 1;
-const classs = 1;
-const _ = 1;
-const a1 = 1;
-const my_name = 1;
-
-var myName1;
-let myname2 = 1;
-let check = ""
-{
-<Input type="text" /> 
+function myFunction() {
+    document.getElementsByName("search").innerHTML = "belum bisa diakses";
 }
+// var myName1;
+// let myName2;
+// const myName3 = "cici";
 
+// console.log(myName1, myName2, myName3);
 
+// myName1 = "desya";
+// myName2 = "eci";
+// // myName3 = "cici2";
+// console.log(myName1, myName2, myName3);
 
-//Biar lebih bingung coba di console 
-//0 == false -> true
-// undefined == null -> true
-//0 == undefined -> false
+// // camel case
+// let myNameInAnotherCountry = 1;
+
+// //snake case
+// let my_name_in_another_country;
+
+// const $ = 1;
+// const classs = 1;
+// const _ = 1;
+// const a1 = 1;
+// const my_name = 1;
+
+// var myName1;
+// let myname2 = 1;
+// let check = ""
+
+// /* <Input type="text"/> */
+
