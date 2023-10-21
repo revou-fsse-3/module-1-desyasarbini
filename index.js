7abca3d20d8093b6e0e739ce597b9f895d2362f6
desyasarbini <desya.sarbini@gmail.com>
2023-10-21 15:02:34 +0800

    update  js

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
+// fungsi untuk alert message
 function myFunction() {
-    document.getElementsByName("search").onclick = function () { myFunction() };
-    document.getElementsByName("search").innerHTML = "belum bisa diakses";
+    alert("Hello! welcome to bookworm sanctuary! what book did you looking for?");
 }
-// masih belom bisa
-// var myName1;
-// let myName2;
-// const myName3 = "cici";
-
-// console.log(myName1, myName2, myName3);
-
-// myName1 = "desya";
-// myName2 = "eci";
-// // myName3 = "cici2";
-// console.log(myName1, myName2, myName3);
-
-// // camel case
-// let myNameInAnotherCountry = 1;
-
-// //snake case
-// let my_name_in_another_country;
-
-// const $ = 1;
-// const classs = 1;
-// const _ = 1;
-// const a1 = 1;
-// const my_name = 1;
-
-// var myName1;
-// let myname2 = 1;
-// let check = ""
-
-// /* <Input type="text"/> */
-
